2f1d6c9d58f5111e86f91420829fae4740d45e5e
vibhathkalsara99 <vibhathkrathnayaka99@gmail.com>
2026-03-21 14:58:55 +0530

    chore(react): install react-router-dom

---
+++ src/components/layout/MainLayout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router'
+import Sidebar from './Sidebar'
+
+export default function MainLayout() {
+  return (
+    <div className="flex h-screen overflow-hidden bg-gray-50">
+      <Sidebar />
+      <main className="flex-1 overflow-y-auto p-6">
+        <Outlet />
+      </main>
+    </div>
+  )
+}
