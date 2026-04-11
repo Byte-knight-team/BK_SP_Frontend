099d43779e2d783a66b080e816b94677a2ee496e
DileepaPRA <prabhath0115@gmail.com>
2026-04-11 13:20:51 +0530

    refactor: removed redundant page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,23 @@
-import MainPage from './MainPage';
+import Navbar from '../components/Navbar';
+import HeroSection from '../components/HeroSection';
+import FeaturesSection from '../components/FeaturesSection';
+import StepsSection from '../components/StepsSection';
+import TestimonialsSection from '../components/TestimonialsSection';
+import ExperienceSection from '../components/ExperienceSection';
+import CTASection from '../components/CTASection';
+import Footer from '../components/Footer';
 
 export default function HomePage() {
-  return <MainPage />;
+  return (
+    <>
+      <Navbar />
+      <HeroSection />
+      <FeaturesSection />
+      <StepsSection />
+      <TestimonialsSection />
+      <ExperienceSection />
+      <CTASection />
+      <Footer />
+    </>
+  );
 }
