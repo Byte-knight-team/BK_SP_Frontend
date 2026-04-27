d797e20797ee1e03a8c91bd9e02563b6331a4d8f
Isuru Adikaram <isadikaram2915@gmail.com>
2026-04-27 11:31:06 +0530

    feat: chef cannot start the meal until assign a chef to the meal. button is desabled and added styles

--- src/components/kitchen/orders/MealTable.jsx
+++ src/components/kitchen/orders/MealTable.jsx
@@ -87,7 +87,13 @@ const MealTable = ({
                         </button>
                         <button
                           onClick={() => onStartMeal(meal.id)}
-                          className="flex items-center gap-1 rounded-xl border border-green-100 px-3 py-2 text-xs font-bold text-green-600 transition-all hover:bg-green-50"
+                          // Check if a chef is assigned. If not, disable the button
+                          disabled={meal.chefName === "Not Assigned"}
+                          className={`flex items-center gap-1 rounded-xl border px-3 py-2 text-xs font-bold transition-all ${
+                            meal.chefName === "Not Assigned"
+                              ? "cursor-not-allowed border-gray-100 bg-gray-50/50 text-gray-300" // Disabled Style
+                              : "border-green-100 text-green-600 hover:bg-green-50" // Enabled Style
+                          }`}
                         >
                           <Play size={14} /> Start
                         </button>
