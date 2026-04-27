f9f62b935e263c50c240572e5f90dcad173c581a
Isuru Adikaram <isadikaram2915@gmail.com>
2026-04-27 18:53:28 +0530

    add two buttons for check in and check out

--- src/pages/kitchen/ChefsPage.jsx
+++ src/pages/kitchen/ChefsPage.jsx
@@ -1,5 +1,5 @@
 import Stats from "../../components/kitchen/chefs/Stats";
-import { ChefHat } from "lucide-react";
+import { ChefHat, LogIn, LogOut } from "lucide-react";
 import ChefsDetailsTable from "../../components/kitchen/chefs/ChefsDetailsTable";
 import { useOutletContext } from "react-router-dom";
 import { useEffect } from "react";
@@ -11,16 +11,41 @@ const ChefsPage = () => {
     // set the header info for this page
     setHeaderInfo({
       title: "Chefs Management",
-      description: "Manage chef profiles, track assignments, and monitor performance metrics.",
+      description: "Manage chef profiles, track assignemnts, and monitor performance metrics.",
       Icon: ChefHat,
     });
   }, []);
 
   return (
     <div className="flex min-h-screen flex-col bg-gray-50 p-4">
-      <div className="mt-6 grid grid-cols-5 gap-4">
+      <div className="mt-6 grid grid-cols-4 gap-4">
         <Stats />
       </div>
+            {/* 2. Action Buttons Section */}
+      <div className="mt-6 flex gap-4 justify-end">
+        {/* Check In Button */}
+        <button 
+          onClick={() => console.log("Check In Clicked")}
+          className="flex item-center gap-2 p-3 rounded-xl border border-orange-100 bg"
+        >
+          <LogIn size={24} />
+          <div className="text-left">
+            <p className="text-sm font-bold">Chef Check-In</p>
+          </div>
+        </button>
+
+        {/* Check Out Button */}
+        <button 
+          onClick={() => console.log("Check Out Clicked")}
+          className="flex item-center gap-2 p-3 rounded-xl border border-orange-100"
+        >
+          <LogOut size={24} />
+          <div className="text-left">
+            <p className="text-sm font-bold">Chef Check-Out</p>
+          </div>
+        </button>
+      </div>
+
       <div className="mt-6 flex flex-1 rounded-2xl bg-white p-4">
         <div className="flex w-full flex-col gap-4">
           <h1 className="text-2xl font-bold tracking-tight text-gray-900">
