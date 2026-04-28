4bb16bfaf7e7156c6aaa5ce4542b19eeedfa780f
Isuru Adikaram <isadikaram2915@gmail.com>
2026-04-28 14:16:47 +0530

    remove check in check out buttons and add styles to the chef management table

--- src/pages/kitchen/ChefsPage.jsx
+++ src/pages/kitchen/ChefsPage.jsx
@@ -21,30 +21,6 @@ const ChefsPage = () => {
       <div className="mt-6 grid grid-cols-4 gap-4">
         <Stats />
       </div>
-            {/* 2. Action Buttons Section */}
-      <div className="mt-6 flex gap-4 justify-end gap-3">
-        {/* Check In Button */}
-        <button 
-          onClick={() => console.log("Check In Clicked")}
-          className="flex item-center gap-2 p-3 rounded-xl border border-orange-100 bg-white text-sm text-orange-600 transition-all hover:bg-orange-50 shadow-sm"
-        >
-          <LogIn size={24} />
-          <div className="text-left">
-            <p className="text-sm font-bold">Chef Check-In</p>
-          </div>
-        </button>
-
-        {/* Check Out Button */}
-        <button 
-          onClick={() => console.log("Check Out Clicked")}
-          className="flex item-center gap-2 p-3 rounded-xl border border-orange-100 bg-white text-sm text-orange-600 transition-all hover:bg-orange-50 shadow-sm"
-        >
-          <LogOut size={24} />
-          <div className="text-left">
-            <p className="text-sm font-bold">Chef Check-Out</p>
-          </div>
-        </button>
-      </div>
 
       <div className="mt-6 flex flex-1 rounded-2xl bg-white p-4">
         <div className="flex w-full flex-col gap-4">
